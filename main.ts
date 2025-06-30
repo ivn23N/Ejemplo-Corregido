917c8a05ba55d0dc1ca7c079986965ca8e482eb6
ivn23N <137209155+ivn23N@users.noreply.github.com>
2025-06-30 12:25:53 +0200

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+//Deploy de prueba, ejemplo Back-end corregido
+
+import { ApolloServer } from "@apollo/server";
+import { schema } from "./schema.ts";
+import { MongoClient } from "mongodb";
+import { UserModel } from "./types.ts";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers.ts";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if (!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("users");
+const UserCollection = mongoDB.collection<UserModel>("users");
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+ context: async () => ({ UserCollection }),
+});
+
+console.info(`Server ready at ${url}`);
